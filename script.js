a54b48bbc252ec04442c13e70530d6f78a033b8d
rokcy154515-clou <rokcy154515@gmail.com>
2025-10-02 12:59:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ function dateFunction(){
     date.innerText = now.toLocaleDateString("en-GB",options)
 }
 function timeFunction(){
-    const now = new Date()
-    let HH=(now.getHours() > 12 ? now.getHours()%12 : now.getHours()).toString().padStart(2,"0")
-    let MM=now.getMinutes().toString().padStart(2,"0")
-    let SS=now.getSeconds().toString().padStart(2,"0")
-    let ampm=now.getHours() >=12 && now.getHours()<24 ? "PM" : "AM"
-    time.innerText=`${HH}:${MM}:${SS} ${ampm}`
+        const now = new Date()
+    
+    let rawH = now.getHours()
+    let HH = rawH % 12 || 12  // converts 0 → 12 and 12 stays 12
+    HH = HH.toString().padStart(2,"0")
+
+    let MM = now.getMinutes().toString().padStart(2,"0")
+    let SS = now.getSeconds().toString().padStart(2,"0")
+
+    let ampm = rawH >= 12 ? "PM" : "AM"
+
+    time.innerText = `${HH}:${MM}:${SS} ${ampm}`
     dateFunction()
 }
 timeFunction()
+
 setInterval(timeFunction,1000)
